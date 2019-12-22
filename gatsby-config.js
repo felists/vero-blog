2ffef7493771536fcf4fbcddb238675fc2242a95
Felisitas Sindiana <felis.sts@gmail.com>
2019-12-22 23:09:46 +0700

    min set

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,17 +9,13 @@ module.exports = {
 
   // Customize your site metadata:
   siteMetadata: {
-    title: `Idea dumps`,
+    title: `Veronica's Blog`,
     author: `vernath`,
     description: `This is where i'm dumping my random ideas`,
     social: [
       {
-        name: `twitter`,
-        url: `https://twitter.com/gatsbyjs`,
-      },
-      {
-        name: `github`,
-        url: `https://github.com/gatsbyjs`,
+        name: `instagram`,
+        url: `https://twitter.com/vrncnthsy`,
       },
     ],
   },
--- src/gatsby-theme-blog/components/bio-content.js
+++ src/gatsby-theme-blog/components/bio-content.js
@@ -7,10 +7,8 @@ import { Styled } from "theme-ui"
 
 export default () => (
   <Fragment>
-    This is where <Styled.a href="http://example.com/">your name</Styled.a>
-    {` `}
-    goes.
+    Hi! I'm <b>Veronica Nathasya</b>, 
     <br />
-    Or whatever, you make the rules.
+    a communication student from Universitas Indonesia.
   </Fragment>
 )
